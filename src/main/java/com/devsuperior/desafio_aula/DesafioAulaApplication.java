a48e8c6b540a8cd0830f9d9a7f2f25e503b4950b
wrffilho <wanderlei.filho@prodam.gov.br>
2024-07-01 18:33:50 -0400

    primeiro commit estrutura inicial do projeto

---
+++ src/main/java/com/devsuperior/desafio_aula/DesafioAulaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio_aula/DesafioAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
